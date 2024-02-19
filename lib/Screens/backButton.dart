4848efeabca7fd1d1a176a30c24bd8eae87ef681
shakya1215 <150770488+shakya1215@users.noreply.github.com>
2024-02-19 17:32:42 +0530

    added the detail screen

---
+++ lib/Screens/backButton.dart
@@ -0,0 +1,30 @@
+import 'package:flutflix/colors.dart';
+import 'package:flutter/material.dart';
+
+class backButton extends StatelessWidget {
+  const backButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      width: 70,
+      margin: const EdgeInsets.only(
+        top: 8,
+        left: 8,   
+      ),
+      decoration:  BoxDecoration(
+        color: Colours.scaffoldBgColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: IconButton(
+        onPressed:(){
+          Navigator.pop(context);
+        } ,
+        icon: const Icon(Icons.arrow_back_rounded),
+      ),
+           );
+  }
+}
